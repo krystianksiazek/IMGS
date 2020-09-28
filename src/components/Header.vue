17532c93b5709d2749dd0b5f671a8ca8fc692760
Krystian Książek <47821226+krystianksiazek@users.noreply.github.com>
2020-09-28 23:50:42 +0200

    Headers moved to separate components.

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wrapper">
+    <h1>The IMGS.</h1>
+    <h2>NASA's space image search engine.</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+<style lang="scss" scoped>
+.wrapper {
+  margin-top: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+h1, h2 {
+  color: white;
+  text-shadow: 0 0 20px #FFFFFF;
+}
+</style>
