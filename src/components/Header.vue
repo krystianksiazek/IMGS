c33b5910e9be52eabb192bbcd8a5b1048536c4c1
Krystian Książek <47821226+krystianksiazek@users.noreply.github.com>
2020-10-04 21:04:55 +0200

    Trouble with Atom Git.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="wrapper">
+  <div v-bind:class="{'startExploringWrapper': (sendState === '1')}" class="wrapper">
     <h1 class="heading">The IMGS</h1>
-    <h2 class="subheading">NASA's space image search engine.</h2>
+    <h2 v-if="sendState === '0'" class="subheading">Discover amazing
+      photos of space objects from NASA</h2>
   </div>
 </template>
 <script>
 export default {
   name: 'Header',
+  props: [
+    'sendState',
+  ],
 };
 </script>
 <style lang="scss" scoped>
 .wrapper {
-  display: flex;
-  flex-direction: column;
+  transition: top 1s ease-out;
+  transition: position 1s ease-out;
+  top: 20%;
 }
 .heading {
   font-size: 60px;
@@ -22,5 +27,13 @@ export default {
   color: white;
   text-shadow: 0 0 10px rgb(200, 200, 200);
   text-align: center;
+}
+.subheading {
+  font-weight: 500;
+}
+.startExploringWrapper {
+  transition: top 1s ease-out;
+  top: 10px;
+  position: absolute;
 }
 </style>
