645d651fcf494d233b3e91566f41e2d093610a7c
Krystian Książek <47821226+krystianksiazek@users.noreply.github.com>
2020-10-06 22:36:40 +0200

    The initial version of the animation is not perfect yet.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,10 @@
 <template>
-  <div v-bind:class="{'startExploringWrapper': (sendState === '1')}" class="wrapper">
-    <h1 class="heading">The IMGS</h1>
-    <h2 v-if="sendState === '0'" class="subheading">Discover amazing
-      photos of space objects from NASA</h2>
+  <div class="wrapper">
+    <h1 v-bind:class="{'exploringSubheading': (sendState === '1')}" class="heading">The IMGS</h1>
+    <h1 v-bind:class="{'exploringSubheading': (sendState === '0')}" class="exploringHeading">
+      IMGS</h1>
+    <h2 v-bind:class="{'exploringSubheading': (sendState === '1')}" class="subheading">
+      Discover amazing photos of space objects from NASA</h2>
   </div>
 </template>
 <script>
@@ -15,13 +17,21 @@ export default {
 </script>
 <style lang="scss" scoped>
 .wrapper {
-  transition: top 1s ease-out;
-  transition: position 1s ease-out;
-  top: 20%;
+}
+.exploringHeading {
+  top: 40px;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  transition: opacity 200ms, visibility 200ms;
+  transition-delay: 0.5s;
 }
 .heading {
   font-size: 60px;
   letter-spacing: 5px;
+  opacity: 1;
+  transition: opacity 200ms, visibility 200ms;
+  font-weight: 500;
 }
 .heading, .subheading {
   color: white;
@@ -29,11 +39,14 @@ export default {
   text-align: center;
 }
 .subheading {
+  visibility: visible;
+  opacity: 1;
+  transition: opacity 200ms, visibility 200ms;
   font-weight: 500;
 }
-.startExploringWrapper {
-  transition: top 1s ease-out;
-  top: 10px;
-  position: absolute;
+.exploringSubheading {
+  visibility: hidden;
+  opacity: 0;
+  transition: opacity 200ms, visibility 200ms;
 }
 </style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,10 +1,13 @@
 <template>
-  <div v-bind:class="{'startExploringWrapper2': (state === '1')}" class="mainWrapper">
+  <div class="mainWrapper">
     <Header :sendState="state" />
     <div class="query">
-      <label v-if="state === '0'" for="search">Type space-related thing to start</label>
+      <label v-bind:class="{'exploringLabel': (state === '1')}" for="search">
+        Type space-related thing to start</label>
       <input
-        v-bind:class="{'startExploringInput': (results.length === 0) && (state === '1')}"
+        v-bind:class="{'inputNegativeResults': (results.length === 0) && (state === '1'),
+        'inputPositiveResults': (results.length > 0) && (state === '1'),
+        'exploringInput': (state === '1')}"
         id="search"
         name="search"
         placeholder="Eg. Saturn"
@@ -13,7 +16,7 @@
         @input="search"
       />
     </div>
-    <p v-if="state === '1'" class="queryStats">Amount of results:
+    <p v-bind:class="{'exploringQueryStats': (state === '1')}" class="queryStats">Amount of results:
       <span v-if="results.length >= 100">
         Above
       </span>
@@ -45,7 +48,8 @@ export default {
     search:
     debounce(function search() {
       this.isLoading = true;
-      if (document.getElementById('search').value === '') {
+      if (document.getElementById('search').value === ''
+      || document.getElementById('search').value === ' ') {
         this.results = [];
         this.state = '0';
         this.isLoading = false;
@@ -70,13 +74,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .startExploringInput {
-    border-bottom: 1px dashed red !important;
-    box-shadow: 0 10px 8px -4px rgba(255, 0, 0, 0.2) !important;
+  .inputNegativeResults {
+    border-bottom: 2px solid rgb(136, 0, 5) !important;
+  }
+  .inputNegativeResults:focus {
+    box-shadow: 0 10px 8px -4px rgba(194, 0, 7, 0.2) !important;
     @media (max-width: 768px)
     {
-      box-shadow: 0 15px 8px -4px rgba(255, 0, 0, 0.2) !important;
+      box-shadow: 0 15px 8px -4px rgba(194, 0, 7, 0.2) !important;
     }
+  }
+  .inputPositiveResults {
+    border-bottom: 2px solid rgb(0, 136, 5) !important;
+  }
+  .inputPositiveResults:focus {
+    box-shadow: 0 10px 8px -4px rgba(0, 194, 7, 0.2) !important;
+    @media (max-width: 768px)
+    {
+      box-shadow: 0 15px 8px -4px rgba(0, 194, 7, 0.2) !important;
+    }
+  }
+  .exploringInput {
+    top: 100px;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    position: absolute;
+    width: 250px;
   }
   .mainWrapper {
     text-shadow: 0 0 10px rgb(200, 200, 200);
@@ -98,17 +121,26 @@ export default {
       font-size: 16px;
       color: white;
       margin-bottom: 10px;
+      visibility: visible;
+      opacity: 1;
+      transition: opacity 200ms, visibility 200ms;
+    }
+    .exploringLabel {
+      visibility: hidden;
+      opacity: 0;
+      transition: opacity 200ms, visibility 200ms;
     }
     input {
       font-weight: 800;
       font-size: 15px;
       border: none;
       height: 30px;
-      border-bottom: 1px dashed white;
+      border-bottom: 2px solid white;
       color: white;
       background-color: rgba(0, 0, 0, 0);
       text-align: center;
       transition: box-shadow .5s ease-out;
+      padding: 0px;
     }
     input:focus {
       outline: none;
@@ -121,5 +153,18 @@ export default {
   }
   .queryStats {
     color: white;
+    visibility: hidden;
+    opacity: 0;
+  }
+  .exploringQueryStats {
+    color: white;
+    visibility: visible;
+    opacity: 1;
+    top: 130px;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    position: absolute;
+    transition: opacity 600ms, visibility 600ms;
+    transition-delay: 0.5s;
   }
 </style>
