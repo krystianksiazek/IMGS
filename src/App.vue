a01a89a990f4fd9950974c7299f01c3576e75222
Krystian Książek <krystianksiazek@users.noreply.github.com>
2021-01-15 18:50:06 +0100

    Fix another bug with starfield

--- src/App.vue
+++ src/App.vue
@@ -80,7 +80,7 @@ export default {
 
 <style lang="scss">
   @import url('https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;500;700&display=swap');
-  #field {
+  #stars {
     position: fixed;
     z-index: 1;
   }
